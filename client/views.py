5866eb19a01c14fd219eeaf74fd81618f951c35e
Xavier <munroexavier@gmail.com>
2019-04-21 18:05:09 -0400

    playing around with templating some more. Gonna start working on this for real after cookbook.

--- client/views.py
+++ client/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-import requests
+import requests as req
 
 def index(request):
-    get_users = requests.get('http://localhost:8000/api/user/')
-    users = get_users.json()
+
+    return render(request, 'client/home.html')
+
+
+def user_detail_view(request, pk=None):
+    get_single_user = req.get(f'http://localhost:8000/api/user/{pk}')
+
+    if get_single_user.status_code == 404:
+        return render(request, 'client/notfound.html')
+
+    single_user = get_single_user.json()
     context = {
-        'users': users
+        'user': single_user
     }
 
-    return render(request, 'client/index.html', context)
+    return render(request, 'client/detail.html', context)
